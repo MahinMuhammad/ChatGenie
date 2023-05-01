1a8039d93b7d41b3b058b3bf0f058fcb512cae03
MahinMuhammad <amimahinurrahman@gmail.com>
2023-05-01 21:11:45 +0600

    custom messagebox design updated

--- ChatGenie/View/CustomMessageCell.swift
+++ ChatGenie/View/CustomMessageCell.swift
@@ -8,11 +8,16 @@
 import UIKit
 
 class CustomMessageCell: UITableViewCell {
-
+    @IBOutlet weak var messageBox: UIView!
+    @IBOutlet weak var userBadge: UIView!
+    @IBOutlet weak var botBadge: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 2.6
+        messageBox.layer.cornerRadius = messageBox.frame.size.height / 2.6
+        userBadge.layer.cornerRadius = userBadge.frame.size.height / 2
+        botBadge.layer.cornerRadius = botBadge.frame.size.height / 2
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
